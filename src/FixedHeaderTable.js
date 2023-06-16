639f749bf5fd761371e576bd42070e2fcb79c8a3
thangtran99hy <127844535+thangtran99hy@users.noreply.github.com>
2023-06-16 10:39:29 +0700

    update

--- src/FixedHeaderTable.js
+++ src/FixedHeaderTable.js
@@ -1,17 +1,19 @@
 import React, { useState } from "react";
-import { Modal, Table } from "antd";
-
+import { Modal, Table, Tag } from "antd";
+import { ArrowLeftOutlined } from "@ant-design/icons";
+import ReactAudioPlayer from "react-audio-player";
 const FixedHeaderTable = (props) => {
     const { items } = props;
     const [openItem, setOpenItem] = useState(null);
     const columns = [
         {
-            title: "index",
+            title: "Index",
             dataIndex: "index",
             key: "index",
+            render: (text) => <Tag color="magenta">{text}</Tag>,
         },
         {
-            title: "title",
+            title: "Title",
             dataIndex: "title",
             key: "title",
         },
@@ -28,19 +30,46 @@ const FixedHeaderTable = (props) => {
                     },
                 })}
             />
-            <Modal
-                open={!!openItem}
-                onCancel={() => {
-                    setOpenItem(null);
-                }}
-                header={null}
-                footer={null}
-            >
-                <div>
-                    <div className="text-lg">{openItem?.title ?? ""}</div>
-                    <div className="text-sm">{openItem?.content ?? ""}</div>
-                </div>
-            </Modal>
+            {!!openItem && (
+                <Modal
+                    open={!!openItem}
+                    onCancel={() => {
+                        setOpenItem(null);
+                    }}
+                    className="fixed top-0 left-0 right-0 bottom-0"
+                    header={null}
+                    footer={null}
+                >
+                    <div className="w-full h-full flex flex-col">
+                        <div className="font-bold mb-2 flex items-center pr-2">
+                            <ArrowLeftOutlined
+                                onClick={() => {
+                                    setOpenItem(null);
+                                }}
+                                className="text-xl cursor-pointer hover:text-gray-400"
+                            />
+                            <div className="px-2">
+                                <Tag color="magenta">{openItem?.index}</Tag>
+                            </div>
+                            <div className="flex-1 text-xl">
+                                {openItem?.title ?? ""}
+                            </div>
+                        </div>
+                        <div className="flex-1 text-md p-1 overflow-y-auto">
+                            {openItem?.content.split("\n").map((item) => {
+                                return <p className="mb-3 leading-7">{item}</p>;
+                            })}
+                        </div>
+                        <div className="flex justify-center mt-2">
+                            <ReactAudioPlayer
+                                src={openItem?.audioUrl}
+                                autoPlay
+                                controls
+                            />
+                        </div>
+                    </div>
+                </Modal>
+            )}
         </div>
     );
 };
