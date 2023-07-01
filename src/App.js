d3b6f634450b2af5172bb890169b3e5755af2a02
thangtran99hy <127844535+thangtran99hy@users.noreply.github.com>
2023-07-01 11:57:45 +0700

    dsd

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import FixedHeaderTable from "./FixedHeaderTable";
-import LogoIcon from "./logo.png"
+import LogoIcon from "./logo.png";
 const App = (props) => {
-    const [items, setItems] = useState(require("./final.json"));
+    const [items, setItems] = useState(require("./read.json"));
     return (
         <div className="App">
             <div className="flex flex-col items-center p-1">
-                <img className="w-[36px] h-[36px]" src={LogoIcon}/>
-                <div className="text-sm font-bold">
-                    Shadowing English App
-                </div>
+                <img className="w-[36px] h-[36px]" src={LogoIcon} />
+                <div className="text-sm font-bold">Shadowing English App</div>
             </div>
             <FixedHeaderTable
                 items={items.map((item, index) => ({
